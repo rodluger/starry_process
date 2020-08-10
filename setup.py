0ed1de1794af2980ec7784bcdac80fc2914e4444
Rodrigo Luger <rodluger@gmail.com>
2020-08-10 10:34:03 -0400

    name change

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-"""Install script for `starry_process`."""
+"""Install script for `starry_gp`."""
 from setuptools import setup
 
 setup(
-    name="starry_process",
+    name="starry_gp",
     version="0.0.1",
     author="Rodrigo Luger",
     author_email="rodluger@gmail.com",
-    url="https://github.com/rodluger/starry_process",
-    description="starry (gaussian) processes",
+    url="https://github.com/rodluger/starry_gp",
+    description="starry gaussian processes",
     license="MIT",
     packages=["starry_gp"],
     install_requires=["scipy>=1.2.1", "starry>=1.0.0"],
