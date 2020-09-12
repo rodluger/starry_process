d8fe7686021ed99329b270776ec8aeeb1f66ced5
Rodrigo Luger <rodluger@gmail.com>
2020-09-12 18:48:09 -0400

    debug...

--- debug.py
+++ debug.py
@@ -4,34 +4,98 @@ import matplotlib.pyplot as plt
 import theano
 import theano.tensor as tt
 import theano.sparse as ts
+from starry.maps import logger
+
+
+ydeg = 15
+map = starry.Map(ydeg, lazy=True)
+map.load("earth")
+_res = tt.iscalar()
+_theta = tt.dvector()
+logger.info("starting")
 
 
 def _func(res, theta):
-    ydeg = 15
-    map = starry.Map(ydeg, lazy=True)
-    map.load("earth")
-
-    # Compute the Cartesian grid
     xyz = map.ops.compute_moll_grid(res)[-1]
+    return xyz
 
-    # Compute the polynomial basis
+
+func = theano.function(
+    [_res, _theta], _func(_res, _theta), on_unused_input="ignore"
+)
+x = func(300, np.array([0.0]))
+logger.info("1 ok")
+
+
+def _func(res, theta):
+    xyz = map.ops.compute_moll_grid(res)[-1]
     pT = map.ops.pT(xyz[0], xyz[1], xyz[2])
+    return pT
 
-    # If orthographic, rotate the map to the correct frame
+
+func = theano.function(
+    [_res, _theta], _func(_res, _theta), on_unused_input="ignore"
+)
+x = func(300, np.array([0.0]))
+logger.info("2 ok")
+
+
+def _func(res, theta):
+    xyz = map.ops.compute_moll_grid(res)[-1]
+    pT = map.ops.pT(xyz[0], xyz[1], xyz[2])
     Ry = tt.transpose(tt.tile(map.y, [theta.shape[0], 1]))
+    return Ry
 
-    # Change basis to polynomials
+
+func = theano.function(
+    [_res, _theta], _func(_res, _theta), on_unused_input="ignore"
+)
+x = func(300, np.array([0.0]))
+logger.info("3 ok")
+
+
+def _func(res, theta):
+    xyz = map.ops.compute_moll_grid(res)[-1]
+    pT = map.ops.pT(xyz[0], xyz[1], xyz[2])
+    Ry = tt.transpose(tt.tile(map.y, [theta.shape[0], 1]))
     A1Ry = ts.dot(map.ops.A1, Ry)
+    return A1Ry
 
-    # Dot the polynomial into the basis
+
+func = theano.function(
+    [_res, _theta], _func(_res, _theta), on_unused_input="ignore"
+)
+x = func(300, np.array([0.0]))
+logger.info("4 ok")
+
+
+def _func(res, theta):
+    xyz = map.ops.compute_moll_grid(res)[-1]
+    pT = map.ops.pT(xyz[0], xyz[1], xyz[2])
+    Ry = tt.transpose(tt.tile(map.y, [theta.shape[0], 1]))
+    A1Ry = ts.dot(map.ops.A1, Ry)
     res = tt.reshape(tt.dot(pT, A1Ry), [res, res, -1])
+    return res
 
-    # We need the shape to be (nframes, npix, npix)
+
+func = theano.function(
+    [_res, _theta], _func(_res, _theta), on_unused_input="ignore"
+)
+x = func(300, np.array([0.0]))
+logger.info("5 ok")
+
+
+def _func(res, theta):
+    xyz = map.ops.compute_moll_grid(res)[-1]
+    pT = map.ops.pT(xyz[0], xyz[1], xyz[2])
+    Ry = tt.transpose(tt.tile(map.y, [theta.shape[0], 1]))
+    A1Ry = ts.dot(map.ops.A1, Ry)
+    res = tt.reshape(tt.dot(pT, A1Ry), [res, res, -1])
     return res.dimshuffle(2, 0, 1)
 
 
-_res = tt.iscalar()
-_theta = tt.dvector()
-func = theano.function([_res, _theta], _func(_res, _theta))
-
-print(func(300, np.array([0.0])))
+func = theano.function(
+    [_res, _theta], _func(_res, _theta), on_unused_input="ignore"
+)
+x = func(300, np.array([0.0]))
+logger.info("6 ok")
