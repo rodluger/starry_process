a7514b1db9e51990650e7332c152b374efa21aa6
Rodrigo Luger <rodluger@gmail.com>
2020-09-12 17:03:13 -0400

    debug [skip ci]

---
+++ debug.py
@@ -0,0 +1,22 @@
+import starry
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("1")
+map = starry.Map(ydeg, lazy=True)
+print("2")
+map.load("earth")
+print("3")
+x = map.render(projection="moll").eval()
+print(x)
+print("4")
+
+
+print("1")
+map = starry.Map(ydeg, lazy=False)
+print("2")
+map.load("earth")
+print("3")
+x = map.render(projection="moll")
+print(x)
+print("4")
