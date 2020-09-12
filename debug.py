6c7d957d4c49624e8960326efc5252e61d59eea2
Rodrigo Luger <rodluger@gmail.com>
2020-09-12 17:42:40 -0400

    compile in debug mode

--- debug.py
+++ debug.py
@@ -1,11 +1,14 @@
 import starry
 import numpy as np
 import matplotlib.pyplot as plt
-
 from starry.maps import logger
+from theano.compile.debugmode import DebugMode
+
+starry.config.mode = DebugMode(check_isfinite=False)
 
 ydeg = 15
 
+"""
 logger.info("1")
 map = starry.Map(ydeg, lazy=True)
 logger.info("2")
@@ -14,7 +17,7 @@ logger.info("3")
 x = map.render(projection="moll").eval()
 logger.info(x)
 logger.info("4")
-
+"""
 
 logger.info("1")
 map = starry.Map(ydeg, lazy=False)
